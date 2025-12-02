b4bd4daa15f160b10f684a5a2d76252b429e9c96
João Paulo Albuquerque <156144908+JoaoPaulo121212@users.noreply.github.com>
2025-12-02 14:39:09 -0300

    Add files via upload

---
+++ SwiftUITraining/SwiftUITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainingApp.swift
+//  SwiftUITraining
+//
+//  Created by Jota Pe on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
